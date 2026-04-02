c5e32989997b6a7ad3ef5f33d4ce8c2c0ba651c2
Blue-Onion <rawatadityasingh963@gmail.com>
2026-04-02 20:47:21 +0530

    r

--- handler/csv/csv.go
+++ handler/csv/csv.go
@@ -1,7 +1,18 @@
 package csv
+
+import record "github.com/Blue-Onion/MahilAi/handler/Record"
+
+
 func DownloadCsv(){
 
 }
-func GetData(){
-	
+func getData(cam string,date string)(*[]record.Records,error){
+	record,err:=record.ReadEvent(cam,date)
+	if err!=nil{
+		return nil,err
+	}
+	return &record,nil
+}
+func createCsv([]record.Records){
+
 }
