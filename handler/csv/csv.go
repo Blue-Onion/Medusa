558b2b6375db56ca6b07448b923552c25c93f4df
Blue-Onion <rawatadityasingh963@gmail.com>
2026-04-03 20:34:06 +0530

    feat: implement dynamic CSV naming with timestamps and camera-specific identifiers

--- handler/csv/csv.go
+++ handler/csv/csv.go
@@ -6,21 +6,41 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	record "github.com/Blue-Onion/MahilAi/handler/Record"
 )
 
 
-func ExportToCsv(cam string,date string){
-	records,err:=getData(cam,date)
-	if err!=nil{
+func ExportToCsv(cam string, date string) {
+	records, err := getData(cam, date)
+	if err != nil {
 		log.Fatal(err.Error())
 	}
-	err=createCsv(*records)
-	if err!=nil{
+
+	now := time.Now().Format("2006-01-02_15-04-05")
+
+	var name string
+
+	if cam == "" && date == "" {
+		name = fmt.Sprintf("all_events_%s.csv", now)
+
+	} else if cam == "" && date != "" {
+		name = fmt.Sprintf("all_events_%s.csv", date)
+
+	} else if cam != "" && date == "" {
+		name = fmt.Sprintf("%s_events_%s.csv", cam, now)
+
+	} else {
+		name = fmt.Sprintf("%s_events_%s.csv", cam, date)
+	}
+
+	err = createCsv(name, *records)
+	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Println("File is downloaded")
+
+	fmt.Println("CSV file exported:", name)
 }
 func getData(cam string,date string)(*[]record.Records,error){
 	record,err:=record.ReadEvent(cam,date)
@@ -29,23 +49,18 @@ func getData(cam string,date string)(*[]record.Records,error){
 	}
 	return &record,nil
 }
-func createCsv(records []record.Records)error{
-	file, err := os.Create("events.csv")
+func createCsv(fileName string,records []record.Records)error{
+	file, err := os.Create(fileName)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
-
-	// header
 	err = writer.Write([]string{"Camera", "Time", "Event", "Confidence"})
 	if err != nil {
 		return err
 	}
-
-	// rows
 	for _, r := range records {
 		row := []string{
 			r.Camera,
@@ -53,12 +68,10 @@ func createCsv(records []record.Records)error{
 			r.Event,
 			strconv.FormatFloat(r.Confidence, 'f', 2, 64),
 		}
-
 		err = writer.Write(row)
 		if err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
